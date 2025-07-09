795fcc679b68e6218a12cb2a26f5b6a6bda1192c
Sofian Adelin <adelinmihai071@gmail.com>
2025-07-09 19:01:24 +0000

    FIX: Type error: Property 'getAll' does not exist on type 'Promise<ReadonlyRequestCookies>'

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -2,27 +2,25 @@ import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
 export function createClient() {
-  const cookieStore = cookies()
-
-  // Create a server's supabase client with newly configured cookie,
-  // which could be used to maintain user's session
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        getAll() {
-          return cookieStore.getAll()
+        async getAll() {
+          const cookieStore = await cookies();
+          return cookieStore.getAll();
         },
-        setAll(cookiesToSet) {
+        async setAll(cookiesToSet) {
+          const cookieStore = await cookies();
           try {
-            cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options)
-            )
+            cookiesToSet.forEach(({ name, value, options }) => {
+              if (typeof cookieStore.set === 'function') {
+                cookieStore.set(name, value, options);
+              }
+            });
           } catch {
-            // The `setAll` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
+            // ignore
           }
         },
       },
